847f577f8b64020c86784e6129e09cbec8be2676
Rafael Santos <rafaelsantos138@hotmail.com>
2023-08-01 16:42:01 -0300

    Adicionado EnumExtension

---
+++ LocadoraAutomoveis.WinApp/Extensions/EnumExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace LocadoraAutomoveis.WinApp.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string ObterDescricao(this Enum valor)
+        {
+            Type tipo = valor.GetType();
+            string nome = Enum.GetName(tipo, valor);
+            FieldInfo campo = tipo.GetField(nome);
+            DescriptionAttribute atributo = campo?.GetCustomAttribute<DescriptionAttribute>();
+            return atributo?.Description ?? nome;
+        }
+    }
+}

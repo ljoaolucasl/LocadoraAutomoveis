9bef6435858c13ec9ee3388350e9f3e9d85a1ec0
Rafael Santos <rafaelsantos138@hotmail.com>
2023-08-01 15:48:01 -0300

    Camada dominio do modulo de cupom iniciado

---
+++ LocadoraAutomoveis.Dominio/ModuloCupom/Cupom.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraAutomoveis.Dominio.Compartilhado;
+
+namespace LocadoraAutomoveis.Dominio.ModuloCupom
+{
+    public class Cupom : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataValidade { get; set; }
+        public string Parceiro { get; set; }
+
+        public Cupom(string nome, decimal valor, DateTime dataValidade, string parceiro)
+        {
+            Nome = nome;
+            Valor = valor;
+            DataValidade = dataValidade;
+            Parceiro = parceiro;
+        }
+
+        public Cupom()
+        {
+            
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Cupom cupom &&
+                   ID == cupom.ID &&
+                   Nome == cupom.Nome &&
+                   Valor == cupom.Valor &&
+                   DataValidade == cupom.DataValidade &&
+                   Parceiro == cupom.Parceiro;
+        }
+    }
+}
---
+++ LocadoraAutomoveis.Dominio/ModuloCupom/ValidadorCupom.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using LocadoraAutomoveis.Dominio.Extensions;
+using System.Text.RegularExpressions;
+
+namespace LocadoraAutomoveis.Dominio.ModuloCupom
+{
+    public class ValidadorCupom : AbstractValidator<Cupom>
+    {
+        public ValidadorCupom()
+        {
+            RuleFor(c => c.Nome)
+                .MinimumLength(3).WithMessage("'Nome' deve ser maior ou igual a 3 caracteres.")
+                .Custom(ValidarCaractereInvalido)
+                .NotEmpty();
+        }
+
+        public bool ValidarCupomExistente(Cupom cupom, List<Cupom> listaCupons)
+        {
+            return listaCupons.Any(c => string.Equals(c.Nome.RemoverAcento(), cupom.Nome.RemoverAcento(), StringComparison.OrdinalIgnoreCase) && c.ID != cupom.ID);
+        }
+
+        private void ValidarCaractereInvalido(string nome, ValidationContext<Cupom> contexto)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+                return;
+
+            if (!Regex.IsMatch(nome, @"^[\p{L}\p{M}'\s-\d]+$"))
+                contexto.AddFailure("Caractere Inválido");
+        }
+    }
+}

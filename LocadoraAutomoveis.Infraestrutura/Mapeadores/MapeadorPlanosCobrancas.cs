83fb2554a851c547a3ddecafe93e71d4ec99e418
Rafael Santos <rafaelsantos138@hotmail.com>
2023-08-04 16:38:04 -0300

    mapeador iniciado

--- LocadoraAutomoveis.Infraestrutura/Mapeadores/MapeadorPlanosCobrancas.cs
+++ LocadoraAutomoveis.Infraestrutura/Mapeadores/MapeadorPlanosCobrancas.cs
@@ -1,10 +1,20 @@
 ﻿using LocadoraAutomoveis.Dominio.ModuloCupom;
 using LocadoraAutomoveis.Dominio.ModuloPlanosCobrancas;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace LocadoraAutomoveis.Infraestrutura.Mapeadores
 {
     public class MapeadorPlanosCobrancas : IEntityTypeConfiguration<PlanoCobranca>
     {
+        public void Configure(EntityTypeBuilder<PlanoCobranca> builder)
+        {
+            builder.ToTable("TBPlanoCobranca");
+            builder.HasKey(p => p.ID);
+            builder.Property(p => p.ValorDia).IsRequired();
+            builder.Property(p => p.ValorKmRodado).IsRequired();
+            builder.Property(p => p.KmLivre).IsRequired();
+            builder.Property
+        }
     }
 }

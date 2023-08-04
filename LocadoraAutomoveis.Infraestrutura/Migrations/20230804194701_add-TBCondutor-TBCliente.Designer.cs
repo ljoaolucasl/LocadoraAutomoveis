67038f932e59322634bee65db8e805e87bb2e6e4
mateu <mateu@DESKTOP-CK25NLM>
2023-08-04 16:48:12 -0300

    Migrations Cliente e Condutor feito

--- LocadoraAutomoveis.Infraestrutura/Migrations/20230804194701_add-TBCondutor-TBCliente.Designer.cs
+++ LocadoraAutomoveis.Infraestrutura/Migrations/20230804194701_add-TBCondutor-TBCliente.Designer.cs
@@ -4,6 +4,7 @@ using LocadoraAutomoveis.Infraestrutura.Compartilhado;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LocadoraAutomoveis.Infraestrutura.Migrations
 {
     [DbContext(typeof(ContextoDados))]
-    partial class ContextoDadosModelSnapshot : ModelSnapshot
+    [Migration("20230804194701_add-TBCondutor-TBCliente")]
+    partial class addTBCondutorTBCliente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

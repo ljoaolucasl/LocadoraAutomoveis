0d5512157035093bfda9a4a0f765a9ebd40f5d17
ljoaolucasl <claudinojoaolucas@gmail.com>
2023-08-04 16:40:46 -0300

    Corrigido as Migration

---
+++ LocadoraAutomoveis.Infraestrutura/Migrations/20230804193931_Add-Tabelas.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraAutomoveis.Infraestrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBCategoriaAutomoveis",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCategoriaAutomoveis", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBFuncionario",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Admissao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Salario = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBFuncionario", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBParceiro",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBParceiro", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBTaxaEServico",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBTaxaEServico", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBAutomovel",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoriaID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Placa = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Marca = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Cor = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Modelo = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Imagem = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    TipoCombustivel = table.Column<int>(type: "int", nullable: false),
+                    CapacidadeCombustivel = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    Quilometragem = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBAutomovel", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TBAutomovel_TBCategoriaAutomoveis",
+                        column: x => x.CategoriaID,
+                        principalTable: "TBCategoriaAutomoveis",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBCupom",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataValidade = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ParceiroID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QtdUsos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCupom", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TBCupom_TBParceiro",
+                        column: x => x.ParceiroID,
+                        principalTable: "TBParceiro",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBAutomovel_CategoriaID",
+                table: "TBAutomovel",
+                column: "CategoriaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBCupom_ParceiroID",
+                table: "TBCupom",
+                column: "ParceiroID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBAutomovel");
+
+            migrationBuilder.DropTable(
+                name: "TBCupom");
+
+            migrationBuilder.DropTable(
+                name: "TBFuncionario");
+
+            migrationBuilder.DropTable(
+                name: "TBTaxaEServico");
+
+            migrationBuilder.DropTable(
+                name: "TBCategoriaAutomoveis");
+
+            migrationBuilder.DropTable(
+                name: "TBParceiro");
+        }
+    }
+}
